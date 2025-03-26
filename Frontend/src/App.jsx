1429d7ac74ecc697ff901874ac413e91d99adc07
Nekal Singh Salaria <nekalsingh987@gmail.com>
2025-03-26 23:01:38 +0530

    make changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   async function reviewCode() {
     setLoading(true)
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post(`http://localhost:${process.env.PORT}/ai/get-review`, { code })
     setReview(response.data)
     setLoading(false)
   }
